88c8065a0601766a52ad4686dd67973fb34f2f5b
ron248 <fackman248@gmail.com>
2015-12-24 03:25:53 +0200

    new desing

--- app/components/homepage/homepageController.js
+++ app/components/homepage/homepageController.js
@@ -2,20 +2,20 @@ app.controller('homepageController', function($scope, localStorage){
     $scope.systems = [
         {'name':'מערכת1', 'imgName': 'panda'},
         {'name':'מערכת2', 'imgName': 'indic'},
-        {'name':'מערכת3', 'imgName': 'apollo'},
+        {'name':'מערכת3', 'imgName': 'jupiter'},
         {'name':'מערכת4', 'imgName': 'triathlon'},
         {'name':'מערכת5', 'imgName': 'triathlon2'},
-        {'name':'מערכת6', 'imgName': 'jupiter'},
+        {'name':'מערכת6', 'imgName': 'shoev'},
         {'name':'מערכת7', 'imgName': 'canary'},
         {'name':'מערכת8', 'imgName': 'hulk'},
-        {'name':'מערכת9', 'imgName': 'broadway'},
-        {'name':'מערכת10', 'imgName': 'shoev'},
+        {'name':'מערכת9', 'imgName': 'childplay'},
+        {'name':'מערכת10', 'imgName': 'apollo'},
         {'name':'מערכת11', 'imgName': 'date'},
         {'name':'מערכת12', 'imgName': 'termite'},
         {'name':'מערכת13', 'imgName': 'yellow'},
         {'name':'מערכת14', 'imgName': 'crunch'},
         {'name':'מערכת15', 'imgName': 'runway'},
-        {'name':'מערכת16', 'imgName': 'childplay'},
+        {'name':'מערכת16', 'imgName': 'broadway'},
         {'name':'מערכת17', 'imgName': 'DMZ'}
     ];
 
